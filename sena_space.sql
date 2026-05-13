5c9bd13f0b77ca2006fcfb3cfe0c25d8a7913436
Kerny9827 <kdjgrajales@gmail.com>
2026-05-13 18:24:07 -0500

    Se actualiza el archivo del sql

--- sena_space.sql
+++ sena_space.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-05-2026 a las 02:17:43
+-- Tiempo de generación: 14-05-2026 a las 01:23:02
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
